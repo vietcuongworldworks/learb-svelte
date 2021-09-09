7e7c2a6766e587863232076e1611b65af9878963
vietcuong.pham <cuong@world-works.co.jp>
2021-09-09 16:36:08 +0900

    checkbox by array

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -4,8 +4,15 @@
     let color = "blue";
     let age = 27;
 
+    let flag1 = false;
+    let flag2 = false;
+
+    let flag = [];
+
     function handleSubmit() {
         console.log(id, name, color, age);
+        console.log(flag1, flag2);
+        console.log(flag);
     }
 </script>
 
@@ -14,6 +21,22 @@
     <input type="text" placeholder="name" bind:value={name} />
     <input type="text" placeholder="color" bind:value={color} />
     <input type="number" placeholder="age" bind:value={age} />
+
+    <input
+        type="checkbox"
+        bind:group={flag}
+        bind:checked={flag1}
+        value="Flag 01"
+    />
+    Flag 01
+    <input
+        type="checkbox"
+        bind:group={flag}
+        bind:checked={flag2}
+        value="Flag 02"
+    />
+    Flag 02
+
     <button>Add person</button>
 </form>
 
